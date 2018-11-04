ce2c080080dcf8d08d082b39b9885eddde7c816a
sheila freire <sheila.freire@dextra-sw.com>
2018-11-04 18:32:11 -0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,31 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
 
-import { AppComponent } from './app.component'
+import { ROUTES } from './app.routes';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TestComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
